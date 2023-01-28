0dfb6cecbade88e636d82dafb87033fcfe02bc09
aymen <aymen.moulehi@outlook.com>
2023-01-28 09:41:12 +0100

    Database configuration.

---
+++ src/main/java/com/example/kaddemapp/KaddemAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.kaddemapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaddemAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KaddemAppApplication.class, args);
+    }
+
+}
